6ee1ddf4ebf7378285176aa58856905fb8eb3410
Dmitriy <artishev.ds@gmail.com>
2022-01-09 16:09:59 +0300

    - repetition threshold added - command lines arguments processing - exceptions handling

---
+++ StringFileGenerator/DetailedMessageException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFileGenerator
+{
+    /// <summary>
+    /// Custom exception for detailed error messages
+    /// </summary>
+    public class DetailedMessageException : Exception
+    {
+        /// <summary>
+        /// Custom exception for detailed error messages
+        /// </summary>
+        /// <param name="message">Clear message for a user</param>
+        /// <param name="inner">Original exception</param>
+        public DetailedMessageException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
--- StringFileGenerator/Program.cs
+++ StringFileGenerator/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using StringFileGenerator.Resources;
+using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StringFileGenerator
@@ -8,27 +10,145 @@ namespace StringFileGenerator
     class Program
     {
         static StringFileGenerator stringFileGenerator;
+
+        /// <summary>
+        /// Maximum value of memory used by this process
+        /// </summary>
+        static long maxMemoryUsed = 0;
+
         static async Task<int> Main(string[] args)
         {
-            string outputFilename = "output.txt";
-            long targetSize = (long)1024 * 1024 * 5000;
-
+            int exitCode = 1;
+            Task memUsageTask = null;
             Stopwatch stopWatch = new Stopwatch();
+            bool GracefulCancel = false;
+            var cts = new CancellationTokenSource();
 
-            if (File.Exists(outputFilename))
-                File.Delete(outputFilename);
+            string outputFilename;
+            int targetSize;
+            int repetitionThreshold;
 
-            stopWatch.Start();
+            Console.CancelKeyPress += (s, e) =>
+            {
+                if (!GracefulCancel)
+                    Environment.Exit(1);
+                Console.WriteLine("Canceling...");
+                cts.Cancel();
+                e.Cancel = true;
+                GracefulCancel = false;
+            };
+            try
+            {
+                memUsageTask = CountMaxMem(cts.Token);
+                if (args.Length < 1
+                    || Array.Exists(new string[4] { "/?", "/h", "-help", "--help" }, input => args[0] == input))
+                {
+                    Console.WriteLine(Constants.helpInfo);
+                    return exitCode;
+                }
 
-            stringFileGenerator = new StringFileGenerator(outputFilename, targetSize);
-            Console.WriteLine($"Generating file \"{outputFilename}\" with size {targetSize:n0}");
-            await stringFileGenerator.Generate();
+                if (!(args.Length > 1 && int.TryParse(args[1], out targetSize) && targetSize > 0 && targetSize < 100))
+                {
+                    targetSize = 100;
+                }
 
-            stopWatch.Stop();
+                if (!(args.Length > 2 && int.TryParse(args[2], out repetitionThreshold) && repetitionThreshold > 0 && repetitionThreshold < 100))
+                {
+                    repetitionThreshold = 10;
+                }
+
+                outputFilename = args[0];
+
+                if (outputFilename.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+                {
+                    throw new DetailedMessageException("Invalid output file name", null);
+                }
+
+                if (File.Exists(outputFilename))
+                {
+                    Console.WriteLine($"The output file already exists. Do you want to overwrite it? (y/n)");
+                    var i = Console.ReadKey();
+                    Console.WriteLine(string.Empty);
+                    if (i.Key == ConsoleKey.Y)
+                    {
+                        try
+                        {
+                            File.Delete(outputFilename);
+                        }
+                        catch (IOException ex)
+                        {
+                            throw new DetailedMessageException("Error deleting output file. The output file is in use.", ex);
+                        }
+                        catch (UnauthorizedAccessException ex)
+                        {
+                            throw new DetailedMessageException("Error deleting output file. The program does not have the required permission.", ex);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new DetailedMessageException("Error deleting output file.", ex);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Operation aborted.");
+                        return exitCode;
+                    }
+                }
+
+                stopWatch.Start();
+
+                GracefulCancel = true;
+
+                stringFileGenerator = new StringFileGenerator(outputFilename, (long)1024 * 1024 * targetSize, repetitionThreshold);
+                Console.WriteLine($"Generating file \"{outputFilename}\" with size {targetSize:n0}MB and repetition threshold {repetitionThreshold}%");
+                await stringFileGenerator.Generate();
+
+                exitCode = 0;
+            }
+            catch (DetailedMessageException dme)
+            {
+                Console.WriteLine($"Error: {dme.Message}\r\nDetails:");
+                if (dme.InnerException != null) 
+                    Console.WriteLine(dme.InnerException);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Operation cancelled.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unhandled exception. Please send a content of this screen to the developer: artishev.ds@gmail.com");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                cts.Cancel();
+                stopWatch.Stop();
+            }
+
             TimeSpan ts = stopWatch.Elapsed;
             Console.WriteLine($"Operation RunTime {ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}:{ts.Milliseconds / 10:00}");
 
             return 0;
+        }
+
+        /// <summary>
+        /// Determinates maximum memory usage
+        /// </summary>
+        /// <param name="ct">Cancelation token. Must be cancelled to stop memory analyze</param>
+        /// <returns>Task represents process of memory analyze</returns>
+        static private async Task CountMaxMem(CancellationToken ct)
+        {
+            do
+            {
+                long usedMemory = Process.GetCurrentProcess().PrivateMemorySize64;
+                if (maxMemoryUsed < usedMemory)
+                {
+                    maxMemoryUsed = usedMemory;
+                }
+
+                await Task.Delay(100);
+            } while (!ct.IsCancellationRequested);
         }
     }
 }
